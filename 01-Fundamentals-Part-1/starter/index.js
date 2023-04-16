4261b75bb409f8329af240098902e67ce1f697e4
Gopi Kishan P <gopi1792000@gmail.com>
2023-04-16 12:38:10 +0530

    Coding challenge 1 in Section 2

---
+++ 01-Fundamentals-Part-1/starter/index.js
@@ -0,0 +1,29 @@
+// //Coding Challenge 1
+
+// const markWeight = prompt("Enter Mark's Weight in kg");
+// const markHeight = prompt("Enter Mark's Height in m");
+
+// console.log("Mark's Weight ", markWeight, "kg");
+// console.log("Mark's Height ", markHeight, "m");
+
+// const markBMI = markWeight / (markHeight * markHeight)
+
+// console.log("Mark's BMI = ", markBMI);
+
+// const johnWeight = prompt("Enter John's Weight in kg");
+// const johnHeight = prompt("Enter John's Height in m");
+
+// console.log("John's Weight ", johnWeight, "kg");
+// console.log("John's Height ", johnHeight, "m");
+
+// const johnBMI = johnWeight / (johnHeight * johnHeight)
+
+// console.log("John's BMI = ", johnBMI);
+
+// if(markBMI > johnBMI){
+// 	console.log('Mark has higher BMI than John');
+// }else if(markBMI < johnBMI){
+// 	console.log('John has higher BMI than Mark');
+// }else{
+// 	console.log('Both have Same BMI');
+// }
